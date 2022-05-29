d33c3efa0982467822cf7de9a6445ed479e67540
nuoclanh86 <nuoclanh86@gmail.com>
2022-05-29 14:14:44 +0700

    - temp disable back to main menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -11,7 +11,7 @@ public class MainMenuManager : MonoBehaviour
 
     private void Start()
     {
-        if (!PhotonNetwork.IsConnected)
+        if (!PhotonNetwork.InLobby)
         {
             connecting.SetActive(true);
             multiplayerBtn.SetActive(false);
